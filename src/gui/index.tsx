045f35e102af8870b2a15b6cb085453136ba0f2d
Nicolas Ramz <nicolas.ramz@gmail.com>
2019-10-03 18:43:19 +0200

    Travis integration (#72)
    * ADDED: travis configuration
    * UPDATED: moved Cypress webpack build && server into separate e2e directory
    * FIXED: .travis.yaml -> .travis.yml
    * UPDATED: e2e build tsconfig.json file
    * REMOVED: --watch option in e2e build script
    * ADDED: missing build-tsconfig.json file
    * FIXED: travis script
    * ADDED: missing linux dep for Cypress (Travis)
    * ADDED: Jest tests to Travis
    * FIXED: enables all Jest tests
    * UPDATED: Travis config file with Jest
    * ADDED: Travis Badge

--- src/gui/index.tsx
+++ src/gui/index.tsx
@@ -1,57 +1,57 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import DevTools from 'mobx-react-devtools';
+import DevTools from "mobx-react-devtools";
 import { ReactApp } from "../components/App";
-import { I18nextProvider } from 'react-i18next';
-import { i18next } from '../locale/i18n';
+import { I18nextProvider } from "react-i18next";
+import { i18next } from "../locale/i18n";
 import { SettingsState } from "../state/settingsState";
 import { Provider } from "mobx-react";
-import { DragDropContextProvider } from 'react-dnd';
-import HTML5Backend from 'react-dnd-html5-backend';
+import { DragDropContextProvider } from "react-dnd";
+import HTML5Backend from "react-dnd-html5-backend";
 
 declare var ENV: any;
 
 class App {
-    settingsState: SettingsState;
+  settingsState: SettingsState;
 
-    constructor() {
-        this.settingsState = new SettingsState(ENV.VERSION);
-        if (ENV.NODE_ENV !== 'production') {
-            this.createTestFolder()
-                .then(this.renderApp);
-        } else {
-            this.renderApp()
+  constructor() {
+    this.settingsState = new SettingsState(ENV.VERSION);
+    if (ENV.NODE_ENV !== "production") {
+      this.createTestFolder().then(this.renderApp);
+    } else {
+      this.renderApp();
+    }
+  }
+
+  // debug stuff
+  createTestFolder(): Promise<any> {
+    return new Promise((resolve, reject) => {
+      // Development stuff: create fake directory for testing
+      const exec = require("child_process").exec;
+      exec("/Users/leo/tmp_ftp.sh", (err: Error) => {
+        if (err) {
+          console.log("error preparing fake folders", err);
         }
-    }
 
-    // debug stuff
-    createTestFolder(): Promise<any> {
-        return new Promise((resolve, reject) => {
-            // Development stuff: create fake directory for testing
-            const exec = require('child_process').exec;
-            exec('/Users/leo/tmp_ftp.sh', (err: Error) => {
-                if (err) {
-                    console.log('error preparing fake folders', err);
-                }
+        resolve();
+      });
+    });
+  }
 
-                resolve();
-            });
-        })
-    }
+  renderApp = () => {
+    document.body.classList.add("loaded");
 
-    renderApp = () => {
-        document.body.classList.add('loaded');
-
-        ReactDOM.render(
-            <DragDropContextProvider backend={HTML5Backend}>
-                <I18nextProvider i18n={i18next}>
-                    <Provider settingsState={this.settingsState}>
-                        <ReactApp></ReactApp>
-                    </Provider>
-                </I18nextProvider>
-            </DragDropContextProvider>,
-            document.getElementById('root'));
-    }
+    ReactDOM.render(
+      <DragDropContextProvider backend={HTML5Backend}>
+        <I18nextProvider i18n={i18next}>
+          <Provider settingsState={this.settingsState}>
+            <ReactApp></ReactApp>
+          </Provider>
+        </I18nextProvider>
+      </DragDropContextProvider>,
+      document.getElementById("root")
+    );
+  };
 }
 
 const app = new App();
