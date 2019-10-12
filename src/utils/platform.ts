344a5ad8f79654d1aec4f5ec5dfe0bb1c255ed3b
warpdesign <nicolas.ramz@gmail.com>
2019-10-12 12:42:32 +0200

    FIXED: defaultFolder definition

--- src/utils/platform.ts
+++ src/utils/platform.ts
@@ -1,15 +1,42 @@
 import { platform } from 'process';
 import { release } from 'os';
 import { remote, app } from 'electron';
-// simulate getPath for test environment
-import { getPath } from './test-helpers';
+
+declare var ENV: any;
+
+type App = {getPath: (name:string) => string} | Partial<Electron.App>;
+
+function getDefaultFolder() {
+    let defaultFolder = "";
+
+    if (typeof jest !== 'undefined') {
+        defaultFolder = "";
+    } else {
+        defaultFolder = ENV.NODE_ENV === 'production' ? appInstance.getPath('home') : (platform === "win32" ? appInstance.getPath('temp') : '/tmp/react-explorer');
+    }
+
+    return defaultFolder;
+}
+
+function getAppInstance() : App {
+    
+    let appInstance:App = app || remote && remote.app;
+    
+    if (!appInstance) {
+        const getPath:(name:string) => string = require('./test-helpers').getPath;
+        // simulate getPath for test environment
+        appInstance = {
+            getPath
+        };
+    }
+
+    return appInstance;
+}
+
+const appInstance = getAppInstance();
 
 const META_KEY = 91;
 const CTRL_KEY = 17;
-// test environement doesn't have access to app
-const appInstance = app || remote && remote.app || {
-    getPath
-};
 
 export const isPackage = process.mainModule && process.mainModule.filename.indexOf('app.asar') > -1;
 export const isMac = platform === 'darwin';
@@ -18,9 +45,7 @@ export const isWin = platform === 'win32';
 export const isLinux = platform === 'linux';
 export const metaKeyCode = isMac && META_KEY || CTRL_KEY;
 export const lineEnding = isWin ? '\r\n' : '\n';
-// Folders
-// export const defaultFolder = ENV.NODE_ENV === 'production' ? appInstance.getPath('home') : (platform === "win32" ? appInstance.getPath('temp') : '/tmp/react-explorer');
-export const defaultFolder = '';
+export const defaultFolder = getDefaultFolder();
 export const TMP_DIR = appInstance.getPath('temp');
 export const HOME_DIR = appInstance.getPath('home');
 export const DOWNLOADS_DIR = appInstance.getPath('downloads');
