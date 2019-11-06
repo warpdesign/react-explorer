e5cd886765dc5e83c86a1a81cd8ba0f05f1a6a19
warpdesign <nicolas.ramz@gmail.com>
2019-11-06 23:50:28 +0100

    ADDED: missing icons.ts constant file

---
+++ src/constants/icons.ts
@@ -0,0 +1,16 @@
+import { IconName } from "@blueprintjs/core";
+
+/**
+ * bp3 icon name for special folders
+ */
+const Icons: { [index: string]: IconName } = {
+    DOWNLOADS_DIR: 'download',
+    MUSIC_DIR: 'music',
+    PICTURES_DIR: 'camera',
+    DESKTOP_DIR: 'desktop',
+    DOCS_DIR: 'projects',
+    HOME_DIR: 'home',
+    VIDEOS_DIR: 'video'
+};
+
+export default Icons;
