12b8391e43a5716917ed2545403c94274d9492ff
Nicolas Ramz <nicolas.ramz@gmail.com>
2022-12-08 23:30:30 +0100

    Statusbar: fixed tests
    Also removed native-ext-loader from webpack+package.json since it's not needed anymore.

--- src/components/__tests__/Statusbar.test.tsx
+++ src/components/__tests__/Statusbar.test.tsx
@@ -2,74 +2,96 @@
  * @jest-environment jsdom
  */
 import React from 'react'
-import { screen, setup, render, t } from 'rtl'
-import { Provider } from 'mobx-react'
+import { screen, setup, render, t, waitFor } from 'rtl'
 import { Statusbar } from '../Statusbar'
+import { filterFiles, filterDirs } from '$src/utils/fileUtils'
+import { File } from '$src/services/Fs'
+import { ViewState } from '$src/state/viewState'
+import { FileState } from '$src/state/fileState'
+import { makeObservable, observable, runInAction } from 'mobx'
 
 describe('Statusbar', () => {
-    const appState = {
-        clipboard: {
-            setClipboard: jest.fn(),
+    const cache = makeObservable(
+        {
+            status: 'ok',
+            files: observable<File>([]),
+            toggleHiddenFiles: jest.fn((show: boolean) => {
+                cache.showHiddenFiles = show
+            }),
+            showHiddenFiles: false,
+            path: '/tmp',
+        } as unknown as FileState,
+        {
+            path: observable,
+        },
+    )
+
+    const options = {
+        providerProps: {
+            viewState: {
+                getVisibleCache: () => cache,
+            } as unknown as ViewState,
         },
     }
 
-    const cache = {
-        selected: [],
-        getFS: (): undefined => undefined,
-        files: [],
-    } as { [x: string]: any }
-
-    const viewState = {
-        getVisibleCache: () => cache,
+    const buildStatusBarText = () => {
+        const files = filterFiles(cache.files, cache.showHiddenFiles).length
+        const folders = filterDirs(cache.files, cache.showHiddenFiles).length
+        return `${t('STATUS.FILES', { count: files })}, ${t('STATUS.FOLDERS', {
+            count: folders,
+        })}`
     }
 
-    const Wrapper = () => (
-        <Provider appState={appState} viewState={viewState}>
-            <Statusbar />
-        </Provider>
-    )
-
     beforeEach(() => {
-        cache.selected = []
-        cache.files = []
-        jest.resetAllMocks()
+        cache.status = 'ok'
+        cache.showHiddenFiles = false
+        cache.files.replace([
+            {
+                fullname: 'dir1',
+                isDir: true,
+            } as unknown as File,
+            {
+                fullname: 'foo1',
+                isDir: false,
+            } as unknown as File,
+            {
+                fullname: '.foo2',
+                isDir: false,
+            } as unknown as File,
+        ])
+
+        jest.clearAllMocks()
     })
 
-    it('should display statusbar', () => {
-        render(<Wrapper />)
-        expect(screen.getByRole('textbox')).toHaveValue(
-            `${t('STATUS.FILES', { count: 0 })}, ${t('STATUS.FOLDERS', {
-                count: 0,
-            })}`,
-        )
+    it('should display statusbar text and toggle hidden files button', () => {
+        render(<Statusbar />, options)
+
+        expect(screen.getByRole('textbox')).toHaveValue(buildStatusBarText())
+        expect(screen.getByRole('button')).toBeInTheDocument()
     })
 
-    it('should display the number of files & folders', () => {
-        const files = 10,
-            folders = 5
+    it('should show hidden files when clicking on toggle hidden files button', async () => {
+        const { user } = setup(<Statusbar />, options)
 
-        cache.files = Array(files + folders)
-            .fill({ isDir: false })
-            .fill({ isDir: true }, files)
-        render(<Wrapper />)
-        expect(screen.getByRole('textbox')).toHaveValue(
-            `${t('STATUS.FILES', { count: files })}, ${t('STATUS.FOLDERS', {
-                count: folders,
-            })}`,
-        )
+        await user.click(screen.getByRole('button'))
+
+        expect(screen.getByRole('textbox')).toHaveValue(buildStatusBarText())
     })
 
-    it('clipboard button should be disabled if selection is empty', () => {
-        render(<Wrapper />)
+    it('toggle hidden files button should be disabled if file cache is not valid', () => {
+        cache.status = 'busy'
+        render(<Statusbar />, options)
+
         expect(screen.getByRole('button')).toBeDisabled()
     })
 
-    it('clicking on clipboard button should set clipboard', async () => {
-        // add a fake file to the selcted files
-        cache.selected = [undefined]
-        const { user } = setup(<Wrapper />)
+    it('should not show hidden files when changing directory', async () => {
+        const { user } = setup(<Statusbar />, options)
         await user.click(screen.getByRole('button'))
-        expect(appState.clipboard.setClipboard).toHaveBeenCalledWith(cache)
-        expect(appState.clipboard.setClipboard).toHaveBeenCalledTimes(1)
+
+        expect(cache.showHiddenFiles).toBe(true)
+        // update path
+        runInAction(() => (cache.path = '/foo'))
+        expect(cache.showHiddenFiles).toBe(false)
     })
 })
