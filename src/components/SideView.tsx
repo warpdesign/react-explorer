9638c1da939fc2c1ef4d3fbc1c70e3e1fe7eff11
Nicolas Ramz <nicolas.ramz@gmail.com>
2022-12-13 11:50:08 +0100

    SideView: rewrote using hooks
    NOTE: need to add tests
    
    Also fixed enter key not working on makedir dialog.

--- src/components/SideView.tsx
+++ src/components/SideView.tsx
@@ -1,146 +1,85 @@
 import * as React from 'react'
 import { Icon, Spinner } from '@blueprintjs/core'
-import { inject, Provider, observer } from 'mobx-react'
-import { withTranslation, WithTranslation } from 'react-i18next'
-import {
-    DropTargetSpec,
-    DropTargetConnector,
-    DropTargetMonitor,
-    DropTargetCollector,
-    ConnectDropTarget,
-    DropTarget,
-} from 'react-dnd'
+import { Provider, observer } from 'mobx-react'
+import { useDrop } from 'react-dnd'
 import classNames from 'classnames'
 
 import { Statusbar } from '$src/components/Statusbar'
 import { Toolbar } from '$src/components/Toolbar'
-import { AppState } from '$src/state/appState'
 import { LoginDialog } from '$src/components/dialogs/LoginDialog'
 import { Overlay } from '$src/components/Overlay'
 import { FileTable } from '$src/components/filetable'
 import { TabList } from '$src/components/TabList'
 import { ViewState } from '$src/state/viewState'
-import { CollectedProps, DraggedObject } from '$src/components/filetable/RowRenderer'
+import { DraggedObject } from '$src/components/filetable/RowRenderer'
+import { useStores } from '$src/hooks/useStores'
 
-interface SideViewProps extends WithTranslation {
+interface SideViewProps {
     hide: boolean
     viewState: ViewState
     onPaste: () => void
-    connectDropTarget?: ConnectDropTarget
-    isOver?: boolean
-    canDrop?: boolean
 }
 
-interface InjectedProps extends SideViewProps {
-    appState?: AppState
-}
+export const SideView = observer(({ hide, viewState, onPaste }: SideViewProps) => {
+    const { appState } = useStores('appState')
+    const [{ isOver, canDrop }, drop] = useDrop(
+        () => ({
+            accept: 'file',
+            canDrop: ({ fileState }: DraggedObject) => {
+                const sourceViewId = fileState.viewId
+                const fileCache = viewState.getVisibleCache()
+                return viewState.viewId !== sourceViewId && fileCache.status !== 'busy' && !fileCache.error
+            },
+            drop: (item) => appState.drop(item, viewState.getVisibleCache()),
+            collect: (monitor) => ({
+                isOver: !!monitor.isOver(),
+                canDrop: !!monitor.canDrop(),
+            }),
+        }),
+        [hide],
+    )
 
-const fileTarget: DropTargetSpec<InjectedProps> = {
-    canDrop(props: InjectedProps, monitor): boolean {
-        // prevent drag and drop in same sideview for now
-        const sourceViewId = monitor.getItem().fileState.viewId
-        const viewState = props.viewState
+    const onValidation = (/*dir: string*/): boolean => true
+
+    const onClose = (): void => {
         const fileCache = viewState.getVisibleCache()
-        return props.viewState.viewId !== sourceViewId && fileCache.status !== 'busy' && !fileCache.error
-    },
-    drop(props, monitor, component): void {
-        const item = monitor.getItem()
-        const sideView = component
-        sideView.onDrop(item)
-    },
-}
-
-const collect: DropTargetCollector<CollectedProps, InjectedProps> = (
-    connect: DropTargetConnector,
-    monitor: DropTargetMonitor,
-) => {
-    return {
-        connectDropTarget: connect.dropTarget(),
-        isOver: monitor.isOver(),
-        canDrop: monitor.canDrop(),
+        // doesn't work: it keeps the previous fs
+        fileCache.revertPath()
     }
-}
 
-export const SideViewClass = inject('appState')(
-    observer(
-        class SideViewClass extends React.Component<InjectedProps> {
-            constructor(props: InjectedProps) {
-                super(props)
-            }
+    const fileCache = viewState.getVisibleCache()
+    const active = appState.getViewFromCache(fileCache).isActive
+    const dropAndOver = isOver && canDrop
+    const divId = 'view_' + viewState.viewId
 
-            get injected(): InjectedProps {
-                return this.props as InjectedProps
-            }
+    const activeClass = classNames('sideview', {
+        active,
+        inactive: !active,
+        hidden: hide,
+        dropTarget: dropAndOver,
+        notDropTarget: isOver && !canDrop,
+    })
 
-            onValidation = (/*dir: string*/): boolean => {
-                return true
-            }
+    const needLogin = fileCache.status === 'login'
+    const busy = fileCache.status === 'busy'
+    const dropOverlayIcon = isOver && !canDrop ? 'cross' : 'import'
+    const dropOverlayActive = isOver
 
-            onClose = (): void => {
-                // login cancelled
-                const { viewState } = this.props
-                const fileCache = viewState.getVisibleCache()
-                // doesn't work: it keeps the previous fs
-                fileCache.revertPath()
-            }
-
-            renderSideView(): React.ReactNode {
-                const { viewState, connectDropTarget, canDrop, isOver } = this.props
-                const fileCache = viewState.getVisibleCache()
-                const appState = this.injected.appState
-                const active = appState.getViewFromCache(fileCache).isActive
-                const dropAndOver = isOver && canDrop
-                const divId = 'view_' + viewState.viewId
-
-                const activeClass = classNames('sideview', {
-                    active: active,
-                    inactive: !active,
-                    hidden: this.props.hide,
-                    dropTarget: dropAndOver,
-                    notDropTarget: isOver && !canDrop,
-                })
-
-                const needLogin = fileCache.status === 'login'
-                const busy = fileCache.status === 'busy'
-                const dropOverlayActive = isOver
-                const dropOverlayIcon = isOver && !canDrop ? 'cross' : 'import'
-
-                return connectDropTarget(
-                    <div id={divId} className={activeClass}>
-                        {needLogin && (
-                            <LoginDialog isOpen={needLogin} onValidation={this.onValidation} onClose={this.onClose} />
-                        )}
-                        <TabList></TabList>
-                        <Toolbar active={active && !busy} onPaste={this.props.onPaste} />
-                        <FileTable hide={this.props.hide} />
-                        <Statusbar />
-                        <Overlay active={busy}>
-                            <Spinner />
-                        </Overlay>
-                        <Overlay active={dropOverlayActive}>
-                            <Icon icon={dropOverlayIcon} size={80} color="#d9dde0" />
-                        </Overlay>
-                    </div>,
-                )
-            }
-
-            onDrop(item: DraggedObject /*| DataTransfer*/) {
-                const { appState, viewState } = this.injected
-                appState.drop(item, viewState.getVisibleCache())
-                debugger
-            }
-
-            render(): React.ReactNode {
-                const { viewState } = this.props
-
-                return <Provider viewState={viewState}>{this.renderSideView()}</Provider>
-            }
-        },
-    ),
-)
-
-const SideViewDD = DropTarget<InjectedProps>('file', fileTarget, collect)(SideViewClass)
-
-const SideView = withTranslation()(SideViewDD)
-
-export { SideView }
+    return (
+        <Provider viewState={viewState}>
+            <div ref={drop} id={divId} className={activeClass}>
+                {needLogin && <LoginDialog isOpen={needLogin} onValidation={onValidation} onClose={onClose} />}
+                <TabList></TabList>
+                <Toolbar active={active && !busy} onPaste={onPaste} />
+                <FileTable hide={hide} />
+                <Statusbar />
+                <Overlay active={busy}>
+                    <Spinner />
+                </Overlay>
+                <Overlay active={dropOverlayActive}>
+                    <Icon icon={dropOverlayIcon} size={80} color="#d9dde0" />
+                </Overlay>
+            </div>
+        </Provider>
+    )
+})
