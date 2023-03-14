730206f57132ff9b48ba31689a832ef684554e58
Nicolas Ramz <nicolas.ramz@gmail.com>
2023-03-14 17:54:49 +0100

    IconView: fixed getNextIndex calculation (#418)
    NumRows was missing from the list of useImperativeHandle keyboard
    navigation was broken when changing directory if it had more/less
    rows.

--- src/components/viewmodes/IconViewMode/index.tsx
+++ src/components/viewmodes/IconViewMode/index.tsx
@@ -107,7 +107,7 @@ export const IconViewMode = forwardRef<ViewModeActions, ViewModeProps<IconViewMo
                 },
                 icons: true,
             }),
-            [itemsPerRow],
+            [itemsPerRow, numRows],
         )
 
         useEffect(() => {
